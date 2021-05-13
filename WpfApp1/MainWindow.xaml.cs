0fd4a798e96f7c21e6b394b8b0848f7904f810a8
mark save <markbooksavenew@gmail.com>
2021-05-13 14:27:26 +0900

    두번쨰

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApp1
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text = "hey";
+            textBlock.Text = "heytest test test 연습 git";
             
         }
     }
